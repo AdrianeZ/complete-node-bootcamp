bb7a9961f50086f7e64ae7e781a8d8e1961e925a
AdrianeZ <eddy122394@gmail.com>
2021-11-18 21:26:57 +0100

    added global error handler

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const toursRouter = require("./routes/tours");
 const usersRouter = require("./routes/users");
+const AppError = require("./utils/AppError");
+const {errorHandler} = require("./controllers/errorController")
 
 const app = express();
-
-
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/tours", toursRouter);
 app.use("/api/users", usersRouter);
 
-app.all("*", (req, res) =>
-{
-  res.status(404).json(
-      {
-        status:"fail",
-        message: `Can't find ${req.originalUrl}`
-      }
-  );
+app.all("*", (req, res, next) => {
+    // next(new AppError(`Can't find ${req.originalUrl} Route`, 404));
+    throw new AppError(`Can't find ${req.originalUrl} Route`, 404);
+
 })
+
+
+app.use(errorHandler);
 
 module.exports = app;
